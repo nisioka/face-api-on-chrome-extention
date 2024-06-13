47709e0381cbf1713eb4778cec5a92b1759157d9
nisioka <16322296+nisioka@users.noreply.github.com>
2024-06-13 17:03:55 +0900

    リファクタリング。

--- js/background.js
+++ js/background.js
@@ -1,61 +1,29 @@
-/*
-// 监听的回调
-var callback = function(details) {
-  
-  if (details.type=='image') {
-    console.log(details.type,details);
-  };
-  var headers = details.responseHeaders;
-  // 返回修改后的headers列表
-  return { responseHeaders: headers };
-};
-// 监听哪些内容
-var filter = {
-  urls: ["<all_urls>"]
-};
-console.log(chrome.webRequest)
-
-chrome.webRequest.onCompleted.addListener(callback, filter,["responseHeaders"]);
-
-*/
-
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log(request);
-
-  if (request.type == 'detectAllFaces_background') {
+  if (request.type === 'detectAllFaces_background') {
     getResult(request.imgurls).then(sendResponse);
-  };
-
+  }
   return true;
-
 });
 
-
-const faceDetectionOptions = new faceapi.SsdMobilenetv1Options({ minConfidence: 0.4 });
-console.log(faceapi.nets.ssdMobilenetv1);
-
+const faceDetectionOptions = new faceapi.SsdMobilenetv1Options(
+    {minConfidence: 0.4});
 
 if (!faceapi.nets.ssdMobilenetv1.params) {
   setTimeout(downloadModel, 500);
 } else {
   console.log('models ready', faceapi.nets)
-};
+}
 
 async function downloadModel() {
   if (!faceapi.nets.ssdMobilenetv1.params) {
-    console.log('download models')
-    const net = await faceapi.createSsdMobilenetv1(await faceapi.fetchNetWeights(chrome.runtime.getURL('/weights/ssd_mobilenetv1.weights')));
-    faceapi.nets.ssdMobilenetv1 = net;
-    console.log('models ready', faceapi.nets.ssdMobilenetv1.params)
-  } else {
-    console.log('models ready', faceapi.nets.ssdMobilenetv1.params)
-  };
-};
-
+    faceapi.nets.ssdMobilenetv1 = await faceapi.createSsdMobilenetv1(
+        await faceapi.fetchNetWeights(
+            chrome.runtime.getURL('/weights/ssd_mobilenetv1.weights')));
+  }
+}
 
 const imageAll = new ImageAll();
-var result = {};
+let result = {};
 
 async function getResult(_urls) {
 
@@ -66,18 +34,18 @@ async function getResult(_urls) {
     console.log(result[url]);
     if (!result[url]) {
       let imgNew = await imageAll.loadImgFromHTTP(url);
-      let detections = await faceapi.detectAllFaces(imgNew, faceDetectionOptions);
+      let detections = await faceapi.detectAllFaces(imgNew,
+          faceDetectionOptions);
       console.log(detections);
       result[url] = detections;
       res[url] = detections;
     } else {
       res[url] = result[url];
-    };
-
-  };
+    }
+  }
 
   return res;
-};
+}
 
 
 
--- js/content_script.js
+++ js/content_script.js
@@ -1,21 +1,19 @@
 async function detectAllFaces_background(_urls) {
-    return new Promise((resolve, reject) => {
-        //console.log(_urls);
+    return new Promise((resolve) => {
         chrome.runtime.sendMessage(
             { type: 'detectAllFaces_background', imgurls: _urls },
             function (response) {
-                //console.log('收到来自后台的回复：', response);
                 resolve(response);
             }
         );
     })
-};
+}
 
 async function init(){
-    const imageAll=new ImageAll(detectAllFaces_background,false);
+    const imageAll=new ImageAll(detectAllFaces_background);
 
     await imageAll.run(document.body);
-};
+}
 
 setTimeout(()=>{
     init();
